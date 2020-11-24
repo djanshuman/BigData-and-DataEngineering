b4ef1f79954ed6bb4de02aa1971db9f29038da89
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2020-11-24 14:51:14 +0530

    Create ClassicWordCountWithCombiner.java
    #Execution
    hadoop jar /Users/dibyajyoti/Desktop/ClassicWordCountWithCombiner.jar  org.hadoop.learning.ClassicWordCountWithCombiner /user/resources/word /user/resources/wc6
    
    	Map-Reduce Framework
    		Map input records=4
    		Map output records=29
    		Map output bytes=274
    		Map output materialized bytes=86
    		Input split bytes=106
    		Combine input records=29
    		Combine output records=7
    
    
    Dibyajyotis-MacBook-Air:hadoop dibyajyoti$ hadoop fs -cat /user/resources/wc6/part-r-00000
    2020-11-24 14:48:58,853 WARN util.NativeCodeLoader: Unable to load native-hadoop library for your platform... using builtin-java classes where applicable
    FRANK	7
    JOHN	8
    KUOA	4
    LEXA	1
    LUPA	3
    SMITH	1
    STEVE	5

---
+++ Map-Reduce/ClassicWordCountWithCombiner.java
@@ -0,0 +1,89 @@
+package org.hadoop.learning;
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.hadoop.fs.Path;
+
+import org.apache.hadoop.io.IntWritable;
+
+import org.apache.hadoop.io.LongWritable;
+
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Job;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class ClassicWordCountWithCombiner {
+
+	// Driver class configuration added in Main method
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		
+		Configuration c = new Configuration();
+		String [] files=new GenericOptionsParser(c,args).getRemainingArgs();
+		Path input= new Path(files[0]);
+		Path output=new Path(files[1]);
+		Job j = Job.getInstance(c,"ClassicWordCountWithCombiner");
+		j.setJarByClass(ClassicWordCountWithCombiner.class);
+		j.setMapperClass(WCMapper.class);
+		j.setReducerClass(WCReducer.class);
+		j.setCombinerClass(CombinerClass.class);
+		j.setOutputKeyClass(Text.class);
+		j.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(j, input);
+		FileOutputFormat.setOutputPath(j, output);
+		System.exit(j.waitForCompletion(true)?1:0);
+	}
+	//Mapper Class
+	public static class WCMapper extends Mapper<LongWritable,Text,Text,IntWritable>{
+		
+		public void map(LongWritable key,Text value,Context con) throws IOException,InterruptedException{
+		
+			String s=value.toString();
+			String [] words=s.split(",");
+			
+			for (String s1 : words) {
+				Text outputkey=new Text(s1.toUpperCase().trim());
+				IntWritable outputvalue=new IntWritable(1);
+				con.write(outputkey, outputvalue);
+			}
+		}
+	}
+	//Reducer Class
+	public static class WCReducer extends Reducer<Text,IntWritable,Text,IntWritable>{
+		
+		public void reduce(Text key,Iterable<IntWritable>values,Context con) throws IOException,InterruptedException{
+		
+			int sum=0;
+			for (IntWritable val: values) {
+				sum+=val.get();
+				
+			}
+			con.write(key, new IntWritable(sum));
+		}
+	}
+	
+	//Combiner Class will be same as Reducer class. We are extending reducer class and adding setCombinerClass in Driver code.
+	public static class CombinerClass extends Reducer<Text,IntWritable,Text,IntWritable>{
+		public void reduce(Text key,Iterable<IntWritable> values,Context con) throws IOException,InterruptedException{
+			
+			int sum=0;
+			for(IntWritable val:values) {
+				sum+=val.get();
+			}
+			con.write(key,new IntWritable(sum));
+		}
+	}
+
+}
+
