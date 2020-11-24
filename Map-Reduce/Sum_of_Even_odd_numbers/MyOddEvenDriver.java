77ad46a939ccfe7b654b8259a6da05cced11bc31
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2020-11-24 16:37:37 +0530

    Update MyOddEvenDriver.java

--- Map-Reduce/Sum_of_Even_odd_numbers/MyOddEvenDriver.java
+++ Map-Reduce/Sum_of_Even_odd_numbers/MyOddEvenDriver.java
@@ -1,18 +1,11 @@
 package org.hadoop.learning;
-
 import org.apache.hadoop.conf.Configuration;
-
 import org.apache.hadoop.fs.Path;
-
 import org.apache.hadoop.io.IntWritable;
-
 import org.apache.hadoop.io.Text;
-
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class MyOddEvenDriver {
