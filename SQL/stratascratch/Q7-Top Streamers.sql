ff3d995da92d530781c34a9f09e7c5061d63b836
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2024-11-01 01:02:41 -0700

    Update Q7-Top Streamers.sql

--- SQL/stratascratch/Q7-Top Streamers.sql
+++ SQL/stratascratch/Q7-Top Streamers.sql
@@ -2,15 +2,30 @@
 https://platform.stratascratch.com/coding/2010-top-streamers?code_type=1
 '''
 
-with cte as (
-select 
-    user_id,
-    count(case
-            when (session_type= 'streamer') then 1
-            else null end ) as streaming,
-    count(case
-            when (session_type= 'viewer') then 1
-            else null end) as view
-    from twitch_sessions
-    group by user_id )
-    select * from cte where streaming > view;
+with cte 
+    as (
+        select * from 
+            (select 
+                 user_id,
+                count(case
+                        when (session_type= 'streamer') then 1
+                        else null end ) as streaming,
+                count(case
+                        when (session_type= 'viewer') then 1
+                        else null end) as view
+                        from twitch_sessions
+                        group by user_id) 
+            a where a.streaming > a.view),
+CTE_final 
+    AS (select
+        user_id,
+        streaming,
+        view,
+        rank() over (order by streaming+view desc) as rnk 
+        from cte
+        )
+            select 
+            user_id,
+            streaming,
+            view
+            from CTE_final where rnk <=10;
