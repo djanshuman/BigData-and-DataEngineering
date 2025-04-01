e641b0694f13439dc27626c356243e709f8b94e4
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2025-04-01 14:59:20 -0700

    Update Q26-Active Users Per Platform.sql

--- SQL/stratascratch/Q26-Active Users Per Platform.sql
+++ SQL/stratascratch/Q26-Active Users Per Platform.sql
@@ -1,11 +1,12 @@
-/*
+'''
 
 Active Users Per Platform
 https://platform.stratascratch.com/coding/2072-active-users-per-platform?code_type=1
 
 For each platform (e.g. Windows, iPhone, iPad etc.), calculate the number of users. Consider unique users and not individual sessions. Output the name of the platform with the corresponding number of users.
 
-*/
+'''
+--solution--
 
 
 select platform,
