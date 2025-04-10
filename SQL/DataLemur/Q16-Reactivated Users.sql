b0a923361232c2c21f8eab3082c1362759c1e8f9
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2025-04-10 16:43:50 -0700

    Update Q16-Reactivated Users.sql

--- SQL/DataLemur/Q16-Reactivated Users.sql
+++ SQL/DataLemur/Q16-Reactivated Users.sql
@@ -67,3 +67,21 @@ mth	reactivated_users
 6	2
 7	1
 """
+
+"""
+Solution - Approach 2
+"""
+
+SELECT 
+  EXTRACT(MONTH FROM curr_month.login_date) AS mth, 
+  count(distinct curr_month.user_id) AS reactivated_users
+FROM user_logins AS curr_month 
+WHERE NOT EXISTS (
+  SELECT * 
+  FROM user_logins AS last_month 
+  WHERE curr_month.user_id = last_month.user_id 
+    AND EXTRACT(MONTH FROM last_month.login_date) = 
+      EXTRACT(MONTH FROM curr_month.login_date - '1 month' :: interval)
+) 
+group by 1
+ORDER BY mth;
