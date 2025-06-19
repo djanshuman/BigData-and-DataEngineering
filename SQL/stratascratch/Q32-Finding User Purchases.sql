1a281eb792b21ee666533e27056d2c20719e70d4
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2025-06-18 20:08:45 -0700

    Update Q32-Finding User Purchases.sql

--- SQL/stratascratch/Q32-Finding User Purchases.sql
+++ SQL/stratascratch/Q32-Finding User Purchases.sql
@@ -4,8 +4,38 @@ Identify returning active users by finding users who made a second purchase with
 
 '''
 
-select 
+
+with f_purchase as (
+select
+    user_id,
+    min(created_at) as "first_purchase"
+    from amazon_transactions
+    group by 1)
+select
     distinct a.user_id
-    from amazon_transactions a inner join amazon_transactions b
-    on a.user_id= b.user_id where a.id <> b.id
-    and b.created_at between a.created_at and a.created_at + interval '7 day';
+    from f_purchase a inner join amazon_transactions b
+    on a.user_id = b.user_id
+    where b.created_at > a.first_purchase
+    and b.created_at between a.first_purchase and a.first_purchase + interval '7 day'
+    order by 1;
+
+'''Output
+View the output in a separate browser tab
+Execution time: 0.00918 seconds
+
+Your Solution:
+user_id
+100
+103
+105
+109
+111
+114
+117
+122
+130
+131
+133
+141
+143
+'''
