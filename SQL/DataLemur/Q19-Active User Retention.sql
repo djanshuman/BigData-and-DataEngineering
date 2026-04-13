b2628297262eab07409e54d9a3ca23c3f810157d
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2026-04-13 00:00:38 -0700

    Update Q19-Active User Retention.sql

--- SQL/DataLemur/Q19-Active User Retention.sql
+++ SQL/DataLemur/Q19-Active User Retention.sql
@@ -26,20 +26,24 @@ Please note that the output provided is for June 2022 as the user_actions table 
 The dataset you are querying against may have different input & output - this is just an example!
   
 """
-SELECT
-  extract(month from event_date) as "mth",
-  count(distinct user_id) as "monthly_active_users"
-  from user_actions as current_month
-  where exists(
-              select * from user_actions as "prev_month"
-              where current_month.user_id = prev_month.user_id
-              and extract(month from prev_month.event_date) 
-              = extract(month from current_month.event_date - interval '1 month')
-        )
-  and event_type in ('sign-in', 'like','comment')
+select 
+  extract(month from event_date) as month,
+  count(distinct user_id) as monthly_active_users
+  from user_actions curr_month
+  where curr_month.event_type in ('sign-in','like','comment')
   and extract(month from event_date) = '7'
   and extract(year from event_date) = '2022'
-  group by 1;
+  and exists (
+              SELECT 
+              1
+              FROM user_actions last_month
+              where curr_month.user_id = last_month.user_id
+              and last_month.event_type in ('sign-in','like','comment')
+              and extract(month from last_month.event_date) = 
+              extract(month from curr_month.event_date - interval '1 month')
+              )
+
+ group by 1;
 
 """
 Output
